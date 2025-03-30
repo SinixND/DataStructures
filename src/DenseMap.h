0feb12c6a181d95cd30415762255915ed22b05d7
SinixND <schaumberger_andreas@gmx.de>
2025-03-30 04:00:15 +0200

    Add interfaced option; Added asserts

--- src/DenseMap.h
+++ src/DenseMap.h
@@ -1,6 +1,8 @@
 #ifndef IG20240128195657
 #define IG20240128195657
 
+#define INTERFACED_DENSEMAP
+
 #include <cassert>
 #include <cstddef>
 #include <unordered_map>
@@ -9,8 +11,24 @@
 
 namespace snx
 {
+#if defined( INTERFACED_DENSEMAP )
+    template <typename Key>
+    class IDenseMap
+    {
+    public:
+        virtual void erase( Key const& key ) = 0;
+        virtual void clear() = 0;
+        virtual bool contains( Key const& key ) const = 0;
+
+        virtual ~IDenseMap() = default;
+    };
+#endif
+
     template <typename Key, typename Type>
     class DenseMap
+#if defined( INTERFACED_DENSEMAP )
+        : public IDenseMap<Key>
+#endif
     {
         //* Vector index is used as value key
         std::vector<Type> values_{};
@@ -23,48 +41,27 @@ namespace snx
 
     public:
         //* ITERATORS
-        auto begin() const
-        {
-            return values_.begin();
-        }
+        auto begin() const { return values_.begin(); }
 
-        auto begin()
-        {
-            return values_.begin();
-        }
+        auto begin() { return values_.begin(); }
 
-        auto end() const
-        {
-            return values_.end();
-        }
+        auto end() const { return values_.end(); }
 
-        auto end()
-        {
-            return values_.end();
-        }
+        auto end() { return values_.end(); }
 
         //* CAPACITY
         //* Return size of DenseMap
-        size_t size() const
-        {
-            return values_.size();
-        }
+        size_t size() const { return values_.size(); }
 
         //* MODIFIERS
-        Type& assign(
-            Key const& key,
-            Type const& value = Type{}
-        )
+        Type& assign( Key const& key, Type const& value = Type{} )
         {
             ( *this ).at( key ) = value;
 
             return ( *this ).at( key );
         }
 
-        Type& insert(
-            Key const& key,
-            Type const& value = Type{}
-        )
+        Type& insert( Key const& key, Type const& value = Type{} )
         {
             if ( keyToIndex_.contains( key ) )
             {
@@ -77,7 +74,8 @@ namespace snx
             //* Add key to valueIndex mapping
             keyToIndex_.insert( { key, valueIndex } );
 
-            //* Add valueIndex to key mapping (internal use only to keep list contiguous)
+            //* Add valueIndex to key mapping (internal use only to keep list
+            // contiguous)
             indexToKey_.push_back( key );
 
             //* Add new value to list
@@ -88,10 +86,7 @@ namespace snx
             return ( *this ).at( key );
         }
 
-        Type& insert_or_assign(
-            Key const& key,
-            Type const& value = Type{}
-        )
+        Type& insert_or_assign( Key const& key, Type const& value = Type{} )
         {
             if ( !keyToIndex_.contains( key ) )
             {
@@ -102,10 +97,7 @@ namespace snx
         }
 
         template <typename... Args>
-        Type& emplace(
-            Key const& key,
-            Args&&... args
-        )
+        Type& emplace( Key const& key, Args&&... args )
         {
             Type value{ std::forward<Args>( args )... };
 
@@ -113,10 +105,7 @@ namespace snx
         }
 
         template <typename... Args>
-        Type& emplace_or_assign(
-            Key const& key,
-            Args&&... args
-        )
+        Type& emplace_or_assign( Key const& key, Args&&... args )
         {
             Type value{ std::forward<Args>( args )... };
 
@@ -129,7 +118,11 @@ namespace snx
         }
 
         //* Moves last value to gap
+#if defined( INTERFACED_DENSEMAP )
+        void erase( Key const& key ) override
+#else
         void erase( Key const& key )
+#endif
         {
             if ( !keyToIndex_.contains( key ) )
             {
@@ -141,7 +134,8 @@ namespace snx
             size_t keptValueIndex{};
             size_t size{ this->size() };
 
-            //* Replace removed value with last value before popping (if more than one value exists) to keep values contiguous
+            //* Replace removed value with last value before popping (if more than one
+            // value exists) to keep values contiguous
             if ( size > 1 )
             {
                 //* Get index of (kept) last value that replaces removed value
@@ -150,7 +144,8 @@ namespace snx
                 //* Get key of replacing/kept value
                 Key keptkey = indexToKey_[keptValueIndex];
 
-                //* Replace (removed) value with kept value (by index) so last entry (duplicate) can be popped (becomes new removed)
+                //* Replace (removed) value with kept value (by index) so last entry
+                //(duplicate) can be popped (becomes new removed)
                 values_[removedValueIndex] = values_[keptValueIndex];
 
                 //* Update value index after replacement
@@ -171,12 +166,11 @@ namespace snx
 
             //* Remove removed value from mapping
             indexToKey_.pop_back();
+
+            assert( ( keyToIndex_.size() == indexToKey_.size() ) && "Internal mismatch!" );
         }
 
-        void changeKey(
-            Key const& from,
-            Key const& to
-        )
+        void changeKey( Key const& from, Key const& to )
         {
             assert( !contains( to ) && "Key already exists, possible loss of data!" );
 
@@ -188,10 +182,16 @@ namespace snx
 
             //* Remap index to new key
             indexToKey_[index( to )] = to;
+
+            assert( ( keyToIndex_.size() == indexToKey_.size() ) && "Internal mismatch!" );
         }
 
         //* Empty all containers
+#if defined( INTERFACED_DENSEMAP )
+        void clear() override
+#else
         void clear()
+#endif
         {
             values_.clear();
             keyToIndex_.clear();
@@ -200,20 +200,14 @@ namespace snx
 
         //* LOOKUP
         //* Access
-        Type const& at( Key const& key ) const
-        {
-            return values_[index( key )];
-        }
+        Type const& at( Key const& key ) const { return values_[index( key )]; }
 
         Type& at( Key const& key )
         {
             return const_cast<Type&>( std::as_const( *this ).at( key ) );
         }
 
-        Type const& operator[]( size_t idx ) const
-        {
-            return values_[idx];
-        }
+        Type const& operator[]( size_t idx ) const { return values_[idx]; }
 
         //* Allow non-const calls
         Type& operator[]( size_t idx )
@@ -222,27 +216,21 @@ namespace snx
         }
 
         bool contains( Key const& key ) const
+#if defined( INTERFACED_DENSEMAP )
+            override
+#endif
         {
             return keyToIndex_.contains( key );
         }
 
         //* Get key for value index
-        Key const& key( size_t idx ) const
-        {
-            return indexToKey_[idx];
-        }
+        Key const& key( size_t idx ) const { return indexToKey_[idx]; }
 
         //* Get value index for key
-        size_t const& index( Key const& key ) const
-        {
-            return keyToIndex_.at( key );
-        }
+        size_t const& index( Key const& key ) const { return keyToIndex_.at( key ); }
 
         //* Return vector (contiguous memory)
-        std::vector<Type> const& values() const
-        {
-            return values_;
-        }
+        std::vector<Type> const& values() const { return values_; }
 
         //* Allow non-const call
         std::vector<Type>& values()
@@ -250,6 +238,6 @@ namespace snx
             return const_cast<std::vector<Type>&>( std::as_const( *this ).values() );
         }
     };
-}
+} // namespace snx
 
 #endif
