a3921c526aa796917208176f279d7ca827eceb7d
SinixND <77335684+SinixND@users.noreply.github.com>
2025-08-01 13:56:26 +0200

    feature(KDT) Implement insert function

--- include/KDTree.h
+++ include/KDTree.h
@@ -14,8 +14,11 @@ namespace snx
     {
         struct Node
         {
-            Id childLeft{ 0 };
-            Id childRight{ 0 };
+            Id children[2]
+            {
+                0,
+                0
+            };
 
             Id dataId{ 0 };
         };
@@ -25,6 +28,9 @@ namespace snx
             KDPosition<K> position{};
             Type value{};
         };
+
+        size_t const LEFT{0};
+        size_t const RIGHT{1};
 
 #if defined( GUI )
     public:
@@ -40,6 +46,125 @@ namespace snx
 
         Id root_{ 1 }; // root nodeId
 
+    public:
+        void insert(
+            Float2 const& position,
+            Type const& value
+        )
+        {
+            //* Check for equal position
+            bool isEqual{true};
+            for (size_t k{0}; k < K) 
+            {
+                if ( data_[nodes_[node].dataId].position[k] != position[k])
+                {
+                    isEqual = false;
+                    break;
+                }
+            }
+
+            if (isEqual) return;
+
+            if (!hasData(root_))
+            {
+                insertData( { position, value } );
+
+                return;
+            }
+
+            insert(
+                position,
+                value,
+                root_,
+                1
+            );
+
+        };
+
+        Float2 getNearestNeighbor(
+            Float2 const& targetPosition
+        )
+        {
+
+        };
+
+    private:
+        bool hasData(
+            Id node
+        ) const
+        {
+            return nodes_[node].dataId;
+        }
+
+        void insertData( Data data)
+        {
+            nodes_.back().dataId = data_.size();
+            data_.emplace_back(
+                data.position,
+                data.value
+            );
+        }
+
+        size_t getDirecion(
+                float newCoordinate, 
+                float oldCoordinate
+            )
+            {
+                return (newCoordinate<oldCoordinate) 
+                    ? LEFT 
+                    : RIGHT;
+            }
+
+        bool hasChild(
+            Id parentNode,
+            size_t direction
+        ) const
+        {
+            return nodes_[parentNode].children[direction];
+        }
+
+        void insertNode(
+            Id node,
+            size_t direction
+        )
+        {
+            nodes_[node].children[direction] = nodes_.size();
+            nodes_.emplace_back();
+        }
+
+        void insert(
+            Float2 const& position,
+            Type const& value,
+            Id node,
+            size_t level
+        )
+        {
+            size_t dimension{level % K};
+            size_t direction{getDirecion(
+                position[k],
+                data_[nodes_[node].dataId].position[k]
+            )};
+
+            if (hasChild(direction))
+            {
+                insert(
+                    position,
+                    value,
+                    nodes_[node].children[direction],
+                    ++level
+                );
+            }
+            else 
+            {
+                insertNode(node, direction);
+                insertData( { position, value } );
+            }
+        };
+
+        Float2 getNearestNeighbor()
+        {
+
+        };
     }
 }
 
